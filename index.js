bc6e3a92b2aa4195846e21faf1110936189afa1c
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2024-09-22 23:25:33 +0330

    env in react app

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ require("dotenv").config();
 // development => https://localhost:3000
 // production => https://api.folan.ir
 
-console.log(process.env.NODE_ENV);
-const baseURL =
-  process.env.NODE_ENV === "development"
-    ? "https://localhost:3000"
-    : "https://api.folan.ir";
-console.log(baseURL);
+// console.log(process.env.NODE_ENV);
+// const baseURL =
+//   process.env.NODE_ENV === "development"
+//     ? "https://localhost:3000"
+//     : "https://api.folan.ir";
+// console.log(baseURL);
+
+console.log(process.env.REACT_APP_PORT);
+console.log(process.env.REACT_APP_BASE_URL);
