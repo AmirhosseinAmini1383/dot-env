c4a6e5403a7dfae262cd10044526565722276c18
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2024-09-22 22:59:07 +0330

    change baseURL with NODE_ENV

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 require("dotenv").config();
-console.log(process.env.PORT);
-console.log(process.env.BASE_URL);
+// console.log(process.env.PORT);
+// console.log(process.env.BASE_URL);
+
+// development => https://localhost:3000
+// production => https://api.folan.ir
+
+console.log(process.env.NODE_ENV);
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "https://localhost:3000"
+    : "https://api.folan.ir";
+console.log(baseURL);
